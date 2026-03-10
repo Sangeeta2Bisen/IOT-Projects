c937d130bad2ba9c82feb3e039aecc38f0174736
Sangeeta2Bisen <sangeetab142007@gmail.com>
2026-03-10 19:57:29 +0530

    Version1

---
+++ LED_ON_IOT.ino
@@ -0,0 +1,11 @@
+//This is My first Programming 
+#define my_led 7 //global Declaration,LED pin no.7
+
+void setup() {
+  pinMode(my_led, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(my_led, HIGH);
+}
